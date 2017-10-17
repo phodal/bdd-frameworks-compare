ff8ec82e3364ac38dd2583351f79ef161a9f4c2e
Phodal HUANG <h@phodal.com>
2017-10-17 16:22:26 +0800

    [GAUGEJS] use rellay case

--- gaugejs/specs/example.spec
+++ gaugejs/specs/example.spec
@@ -5,14 +5,11 @@
 联合国海地稳定特派团成员全数撤出海地，正式结束为期13年的维持和平行动任务。
 在德国法兰克福书展最后一天，加拿大作家玛格丽特·阿特伍德获颁德国书商和平奖。
 
-* Go to Gauge homepage at "http://getgauge.io"
+* 当我在网站的首页
 
-开始做点什么
+正常登录
 -----------
-* Go to Get Started page
-* Show subtitle "Choose your OS & download"
-
-开始做点什么2
------------
-* Go to Get Started page
-* Show subtitle "Choose your OS & download"
+* 输入用户名 "demo"
+* 输入密码 "mode"
+* 提交登录信息
+* 用户应该跳转到欢迎页
--- gaugejs/tests/step_implementation.js
+++ gaugejs/tests/step_implementation.js
@@ -1,30 +1,48 @@
 /* globals gauge*/
 "use strict";
 const puppeteer = require('puppeteer');
-const assert = require("assert");
+const expect = require('chai').expect;
 
 var page;
+var mainFrame;
 var browser;
 
 beforeSuite(async function () {
-  browser = await puppeteer.launch({headless: false});
+  browser = await puppeteer.launch({
+    headless: false,
+    // slowMo: 100
+  });
   page = await browser.newPage();
+  mainFrame = page.mainFrame();
 });
 
 afterSuite(async function () {
   browser.close();
 });
 
-step("Go to Gauge homepage at <query>", async function (query) {
-  await page.goto(query);
+step("当我在网站的首页", async function () {
+  await page.goto('http://0.0.0.0:7272/');
 });
 
-step("Go to Get Started page", async function () {
-  await page.click(".link-get-started");
+step("输入用户名 <query>", async function (query) {
+  await page.click('#username_field');
+  await page.type(query)
 });
 
-step("Show subtitle <title>", async function(title){
-  await page.waitFor('.sub-title');
-  const message = await page.$eval('.sub-title', e => e.innerText);
-  assert.equal(message, title);
+step("输入密码 <query>", async function (query) {
+  await page.click('#password_field');
+  await page.type(query)
+});
+
+step("提交登录信息", async function () {
+  await page.click('#login_button')
+});
+
+step("用户应该跳转到欢迎页", async function(){
+  await page.waitFor('h1');
+  const text = await await page.$eval('#container h1', h1 => {
+    return h1.innerHTML;
+  });
+
+  expect(text).to.equal('Welcome Page');
 });
